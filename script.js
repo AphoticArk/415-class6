a97cd7bd75f40b0959661a542547d45d2560ad47
AphoticArk <mshields.ark@gmail.com>
2024-02-22 15:44:57 -0500

    Trying to get wheel to spin on button

--- script.js
+++ script.js
@@ -3,4 +3,27 @@ console.log("testing");
 console.log("working");
 
 // Variable Declaration
-let buttons = document.querySelectorAll('.button');
+let amountRotate;
+
+let button = document.querySelectorAll('.button');
+
+let wheel = document.getElementById('wheel');
+
+function randomInteger(min, max) {
+     //return a random int between min and max
+     return Math.floor(Math.random() * (max - min + 1)) + min;
+   
+}
+
+function spinWheel() {
+     console.log("spin");
+
+     amountRotate = randomInteger(1800, 2160);
+     console.log("amountRotate");
+
+     for (let i = 0; i < amountRotate; i++) {
+          wheel.style.transform = "rotate(1deg)";
+     }
+}
+
+button.addEventListener('click', spinWheel);
